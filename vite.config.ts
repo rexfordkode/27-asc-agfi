83dd80ed6daca3c5a38ad1ffbb77009e9e8d2c47
Rexfordkode <rexfordk.heatmap@gmail.com>
2026-01-02 23:26:33 -0500

    🔧 Fix: Change base path to / for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
-  base: "/27-asc-agfi/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
